9622aa7326f0c13a103f079f97fcda43090a7fa8
anton bogatchenko <anton@antonbogatchenko.com>
2025-11-10 22:40:35 +0200

    Update images

--- src/app/data.ts
+++ src/app/data.ts
@@ -2,30 +2,69 @@ import { Project } from "./types";
 
 export const projects: Project[] = [
   {
-    name: "Woods Photography",
-    description:
-      "A collection of serene photographs capturing the beauty of the woods.",
+    name: "By The Broken Trough",
+    description: "Modern reflection of an old moral",
     accent: "#ffe45e",
     items: [
-      { src: "/projects/woods/woods_1.jpg", description: "Woods Photo 1" },
-      { src: "/projects/woods/woods_2.jpg", description: "Woods Photo 2" },
-      { src: "/projects/woods/woods_3.jpg", description: "Woods Photo 3" },
-      { src: "/projects/woods/woods_4.jpg", description: "Woods Photo 4" },
-      { src: "/projects/woods/woods_5.jpg", description: "Woods Photo 5" },
+      { src: "/projects/Maria/maria-3.jpg", description: "Maria 01" },
+      { src: "/projects/Maria/maria-5.jpg", description: "Maria 02" },
+      { src: "/projects/Maria/maria-15.jpg", description: "Maria 03" },
     ],
   },
   {
-    name: "Cityscapes",
-    description:
-      "A vibrant series showcasing the dynamic energy of urban life.",
+    name: "Ernest",
+    description: "Images for Tinder profile",
+    accent: "#69d5ff",
+    items: [
+      { src: "/projects/Ernest/Ernest-2.jpg", description: "Ernest 02" },
+      { src: "/projects/Ernest/Ernest-5.jpg", description: "Ernest 05" },
+    ],
+  },
+  {
+    name: "Lobo",
+    description: "Food photography for a Lobo restaurant in Helsinki, Finland.",
     accent: "#69d5ff",
     items: [
       {
-        src: "/projects/woods/woods_5.jpg",
-        description: "Cityscape Photo 1",
+        src: "/projects/Lobo/lobo_boreal_25-07-hero.jpg",
+        description: "Lobo Boreal 01",
       },
-      { src: "/projects/woods/woods_1.jpg", description: "Cityscape Photo 2" },
-      { src: "/projects/woods/woods_4.jpg", description: "Cityscape Photo 3" },
+      {
+        src: "/projects/Lobo/lobo_boreal_25-06-amuse-bouce-hero.jpg",
+        description: "Lobo Boreal 02",
+      },
+      {
+        src: "/projects/Lobo/lobo_boreal_25-3-herring.jpg",
+        description: "Lobo Boreal 03",
+      },
+      {
+        src: "/projects/Lobo/lobo_boreal_25-08-sweet2.jpg",
+        description: "Lobo Boreal 04",
+      },
+      {
+        src: "/projects/Lobo/lobo_boreal_25-03-Felipe1.jpg",
+        description: "Lobo Boreal 05",
+      },
+    ],
+  },
+  {
+    name: "Magu",
+    description:
+      "Dinner party photos for a Magu restaurnat in Helsinki, Finland.",
+    accent: "#69d5ff",
+    items: [
+      {
+        src: "/projects/Magu/Magu_dinner_people_-01.jpg",
+        description: "Magu dinner 01",
+      },
+      {
+        src: "/projects/Magu/Magu_dinner_people_-03.jpg",
+        description: "Magu dinner 02",
+      },
+      {
+        src: "/projects/Magu/Magu_dinner_people_-07.jpg",
+        description: "Magu dinner 03",
+      },
     ],
   },
 ];
